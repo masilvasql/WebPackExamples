70b3d433b661f2735d9462c027d6effa7d123ac0
Marcelo Abrão da Silva <masilvasql@gmail.com>
2018-08-15 21:24:52 -0300

    exemplos web pack

---
+++ ex.1/logger.js
@@ -0,0 +1,5 @@
+function info (text){
+    console.log(`info: ${text}`)
+}
+
+module.exports = {info}
---
+++ ex/index.js
@@ -0,0 +1,10 @@
+import 'react' 
+import './estilo.css'
+
+export default props =>(
+    <h1>Olá</h1>
+)
+    
+
+
+console.log('funcionou')
